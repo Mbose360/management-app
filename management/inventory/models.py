0fb35624aa13e26d02ec86a7060d3917356c9c6e
mbose360 <moatazbillahgahgah@gmail.com>
2025-08-07 00:56:48 +0200

    add product model and fixed some errors

--- management/inventory/models.py
+++ management/inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    Product_name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', null=True ,blank=True)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    
+    def __str__(self):
+        return self.Product_name
