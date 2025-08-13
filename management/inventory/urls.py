83dc8e6f0f61f5fe1d88d73309a8233238af24f3
mbose360 <moatazbillahgahgah@gmail.com>
2025-08-13 22:04:11 +0200

    Add REST API for Product

--- management/inventory/urls.py
+++ management/inventory/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
