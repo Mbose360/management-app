02fe72432d8053a0e8e22f08747e1eef6f161829
mbose360 <moatazbillahgahgah@gmail.com>
2025-08-13 21:29:24 +0200

    add product to admin

--- management/inventory/admin.py
+++ management/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['Product_name', 'quantity', 'unit_price', 'image']
+    search_fields = ['Product_name']
 # Register your models here.
